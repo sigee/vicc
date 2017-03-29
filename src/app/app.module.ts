7f7fd6848a1a378802795d2593e95db49118f49b
Balazs Foldesi <balazs.foldesi@gmail.com>
2017-03-29 16:21:06 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { CategoryViewComponent } from './category-view/category-view.component';
+
+import { DataserviceService } from './dataservice.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    CategoryViewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [DataserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
