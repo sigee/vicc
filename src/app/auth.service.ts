5765daf41b83decd37377befcd63a698272793cc
Balazs Foldesi <balazs.foldesi@gmail.com>
2017-04-09 00:57:04 +0200

    more auth

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -8,8 +8,11 @@ declare var Auth0Lock: any;
 export class AuthService {
   // We'll use the Auth0 Lock widget for capturing user credentials
   lock = new Auth0Lock('Fi8d0QJl68I1Y1DKN9Jsl9fB2hGnxmb0', 'terravibe.eu.auth0.com');
+  //Store profile object in auth class
+  userProfile: Object;
 
   constructor(private router: Router) {
+    this.userProfile = JSON.parse(localStorage.getItem('profile'));
     // We'll listen for an authentication event to be raised and if successful will log the user in.
     this.lock.on('authenticated', (authResult: any) => {
       localStorage.setItem('id_token', authResult.idToken);
@@ -39,7 +42,7 @@ export class AuthService {
     localStorage.removeItem('id_token');
 
     // Send the user back to the public deals page after logout
-    this.router.navigateByUrl('/profil');
+    this.router.navigateByUrl('/');
   }
 
   // Finally, this method will check to see if the user is logged in. We'll be able to tell by checking to see if they have a token and whether that token is valid or not.
