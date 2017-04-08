14707e154189ad4bc57826b46370c47480ccfc61
Balazs Foldesi <balazs.foldesi@gmail.com>
2017-04-09 01:01:01 +0200

    more auth

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../auth.service';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
