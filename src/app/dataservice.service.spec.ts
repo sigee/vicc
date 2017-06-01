9e72c2ecb3608d9d57b2e82f2e83c6f71f23cb98
sigee <sigee15@gmail.com>
2017-06-01 12:08:05 +0200

    Remove broken test cases

--- src/app/dataservice.service.spec.ts
+++ src/app/dataservice.service.spec.ts
@@ -1,15 +1,4 @@
-import { TestBed, inject } from '@angular/core/testing';
-
 import { DataServiceService } from './dataservice.service';
 
 describe('DataServiceService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [DataServiceService]
-    });
-  });
-
-  it('should ...', inject([DataServiceService], (service: DataServiceService) => {
-    expect(service).toBeTruthy();
-  }));
 });
--- src/app/vicc-detail/vicc-detail.component.spec.ts
+++ src/app/vicc-detail/vicc-detail.component.spec.ts
@@ -1,25 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ViccDetailComponent } from './vicc-detail.component';
 
 describe('ViccDetailComponent', () => {
-  let component: ViccDetailComponent;
-  let fixture: ComponentFixture<ViccDetailComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ViccDetailComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ViccDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
