a6ecb9ec64c9ac925b696a883cd1f3e160c146f1
sigee <sigee15@gmail.com>
2017-06-01 18:59:57 +0200

    Add tests for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,27 @@
 import { AppComponent } from './app.component';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 describe('AppComponent', () => {
+  let fakeDataService: any;
+  let fakeAuthService: any;
+  let component: AppComponent;
+
+  beforeEach(() => {
+    fakeDataService = jasmine.createSpyObj(fakeDataService, ['getCategories']);
+    fakeDataService.getCategories.and.returnValue(Observable.of({data: ['FAKE_CATEGORY']}));
+
+    fakeAuthService = jasmine.createSpyObj(fakeAuthService, ['login', 'logout', 'loggedIn']);
+
+    component = new AppComponent(fakeAuthService, fakeDataService);
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined('Whoopie!');
+  });
+
+  it('should set categories property when initialised', () => {
+    component.ngOnInit();
+    expect(component.categories).toEqual(['FAKE_CATEGORY']);
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
-import {DataServiceService} from './dataservice.service';
+import { DataServiceService } from './dataservice.service';
 
 @Component({
   selector: 'app-root',
